7a3e090fe8f9671ae6c6e85cbce29bc68991543f
FelipeCarvalho25 <joao_carvalho@edu.univali.br>
2020-06-22 10:26:59 -0300

    Adicionados layouts e classes de novas telas

---
+++ app/src/main/java/com/example/unicarejfw/LoginActv.java
@@ -0,0 +1,4 @@
+package com.example.unicarejfw;
+
+public class LoginActv {
+}
---
+++ app/src/main/java/com/example/unicarejfw/ui/home/HistoricoActv.java
@@ -0,0 +1,21 @@
+package com.example.unicarejfw.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.unicarejfw.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HistoricoActv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.historico_act);
+    }
+}
