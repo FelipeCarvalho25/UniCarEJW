c213708717b7e298eb6ab5a325a6898a3e229c25
FelipeCarvalho25 <joao_carvalho@edu.univali.br>
2020-07-05 17:44:37 -0300

    Adicionada logica para notificações e adicionado botões do perfil.

--- app/src/main/java/com/example/unicarejfw/MainActivity.java
+++ app/src/main/java/com/example/unicarejfw/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.unicarejfw;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.unicarejfw.ui.perfil.PerfilFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "1706";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +40,41 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
 
 
+    }
+
+    public void criarNotificacao(View view) {
+        // Para as versões de API 26+ é necessário criar o canal da notificação.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence nome = "Unicar";
+            String descricao = "Canal Unicar";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel canal = new NotificationChannel(CHANNEL_ID, nome, importance);
+            canal.setDescription(descricao);
+            NotificationManager nm = getSystemService(NotificationManager.class);
+            nm.createNotificationChannel(canal);
+        }
+
+        Intent i = new Intent(this, PerfilFragment.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent p = PendingIntent.getActivity(this, 0, i, 0);
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentTitle("Unicar")
+                        .setContentText("Toque para abrir seu perfil!")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true)
+                        .setContentIntent(p);
+
+        Notification n = builder.build();
+        n.vibrate = new long[]{150, 300, 150, 600};
+
+        NotificationManagerCompat nmc = NotificationManagerCompat.from(this);
+        nmc.notify(170620, n);
+
+        Uri som = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone toque = RingtoneManager.getRingtone(this, som);
+        toque.play();
     }
 
 }
--- app/src/main/java/com/example/unicarejfw/ui/msg/MsgFragment.java
+++ app/src/main/java/com/example/unicarejfw/ui/msg/MsgFragment.java
@@ -36,4 +36,6 @@ public class MsgFragment extends Fragment {
 
         return root;
     }
+
+
 }
